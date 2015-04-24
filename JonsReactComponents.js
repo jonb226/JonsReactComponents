51eec4387b13729dfa7b14eed304bf9e50bee28d
Jon Boerner <jon@jonboerner.com>
2015-04-23 22:39:11 -0400

    export merge

--- JonsReactComponents.js
+++ JonsReactComponents.js
@@ -4,9 +4,11 @@
 let SlidingTransitionView = require('./SlidingTransitionView.jsx');
 let SlidingTransitionViewPane = require('./SlidingTransitionViewPane.jsx');
 let Toolbar = require('./Toolbar.jsx');
+let merge = require('./merge.js');
 
 module.exports = {
     SlidingTransitionView,
     SlidingTransitionViewPane,
-    Toolbar
+    Toolbar,
+    merge
 };
